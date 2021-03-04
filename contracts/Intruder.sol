d883e15f2fab667bcd79800b753ec991509811bc
yogesh <yrajawat19@gmail.com>
2021-03-05 00:27:35 +0530

    4.Token Sale

--- contracts/Intruder.sol
+++ contracts/Intruder.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.16;
+pragma solidity ^0.5.16;
 contract Intruder {
 	string public name = "Intruder Token";
 	string public symbol = "IGX";
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-const Intruder = artifacts.require("./Intruder.sol");
+var Intruder = artifacts.require("./Intruder.sol");
+var IntruderSale = artifacts.require("./IntruderSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Intruder,1000000);
+  deployer.deploy(Intruder,1000000).then(function(){
+  	//Token price is 0.01 Ether
+  	var tokenPrice = 1000000000000000;
+  	return deployer.deploy(IntruderSale,Intruder.address, tokenPrice);
+  });
 };
